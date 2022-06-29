a893bc02e92b89f58aef08f478acdecdb1e029dc
MarcHajjar25 <108301058+MarcHajjar25@users.noreply.github.com>
2022-06-29 11:44:02 -0500

    Add files via upload

---
+++ Python Loops + Return Functions.py.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def fun():
+    Games = ["Valorent", "Minecraft", "COD", "Fortnite", "CSGO", "Apex Legends"]
+    return games
+for x in Games:
+  print(x)
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
